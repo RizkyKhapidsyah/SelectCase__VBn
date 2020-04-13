5c8d7ddc17a6a75cc2f7a0f0e817b6eb30aeaef1
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-04-13 23:44:54 +0700

    Add project files.

---
+++ BungaDeposito.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BungaDeposito
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelJangkaWaktu = New System.Windows.Forms.Label()
+        Me.TextBoxJangkaWaktu = New System.Windows.Forms.TextBox()
+        Me.LabelPilihan = New System.Windows.Forms.Label()
+        Me.LabelJumlah = New System.Windows.Forms.Label()
+        Me.TextBoxJumlah = New System.Windows.Forms.TextBox()
+        Me.LabelBungaDeposito = New System.Windows.Forms.Label()
+        Me.LabelTotalUangAnda = New System.Windows.Forms.Label()
+        Me.LabelRpTotalUangAnda = New System.Windows.Forms.Label()
+        Me.LabelRpBungaDeposito = New System.Windows.Forms.Label()
+        Me.ltd4 = New System.Windows.Forms.Label()
+        Me.ltd3 = New System.Windows.Forms.Label()
+        Me.ltd2 = New System.Windows.Forms.Label()
+        Me.ltd1 = New System.Windows.Forms.Label()
+        Me.ButtonHitung = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LabelJangkaWaktu
+        '
+        Me.LabelJangkaWaktu.AutoSize = True
+        Me.LabelJangkaWaktu.Location = New System.Drawing.Point(14, 11)
+        Me.LabelJangkaWaktu.Name = "LabelJangkaWaktu"
+        Me.LabelJangkaWaktu.Size = New System.Drawing.Size(39, 13)
+        Me.LabelJangkaWaktu.TabIndex = 0
+        Me.LabelJangkaWaktu.Text = "Label1"
+        '
+        'TextBoxJangkaWaktu
+        '
+        Me.TextBoxJangkaWaktu.Location = New System.Drawing.Point(153, 9)
+        Me.TextBoxJangkaWaktu.Name = "TextBoxJangkaWaktu"
+        Me.TextBoxJangkaWaktu.Size = New System.Drawing.Size(156, 20)
+        Me.TextBoxJangkaWaktu.TabIndex = 1
+        '
+        'LabelPilihan
+        '
+        Me.LabelPilihan.AutoSize = True
+        Me.LabelPilihan.Location = New System.Drawing.Point(150, 32)
+        Me.LabelPilihan.Name = "LabelPilihan"
+        Me.LabelPilihan.Size = New System.Drawing.Size(39, 13)
+        Me.LabelPilihan.TabIndex = 2
+        Me.LabelPilihan.Text = "Label2"
+        '
+        'LabelJumlah
+        '
+        Me.LabelJumlah.AutoSize = True
+        Me.LabelJumlah.Location = New System.Drawing.Point(14, 54)
+        Me.LabelJumlah.Name = "LabelJumlah"
+        Me.LabelJumlah.Size = New System.Drawing.Size(39, 13)
+        Me.LabelJumlah.TabIndex = 3
+        Me.LabelJumlah.Text = "Label3"
+        '
+        'TextBoxJumlah
+        '
+        Me.TextBoxJumlah.Location = New System.Drawing.Point(153, 51)
+        Me.TextBoxJumlah.Name = "TextBoxJumlah"
+        Me.TextBoxJumlah.Size = New System.Drawing.Size(156, 20)
+        Me.TextBoxJumlah.TabIndex = 4
+        '
+        'LabelBungaDeposito
+        '
+        Me.LabelBungaDeposito.AutoSize = True
+        Me.LabelBungaDeposito.Location = New System.Drawing.Point(14, 102)
+        Me.LabelBungaDeposito.Name = "LabelBungaDeposito"
+        Me.LabelBungaDeposito.Size = New System.Drawing.Size(39, 13)
+        Me.LabelBungaDeposito.TabIndex = 5
+        Me.LabelBungaDeposito.Text = "Label4"
+        '
+        'LabelTotalUangAnda
+        '
+        Me.LabelTotalUangAnda.AutoSize = True
+        Me.LabelTotalUangAnda.Location = New System.Drawing.Point(14, 134)
+        Me.LabelTotalUangAnda.Name = "LabelTotalUangAnda"
+        Me.LabelTotalUangAnda.Size = New System.Drawing.Size(39, 13)
+        Me.LabelTotalUangAnda.TabIndex = 6
+        Me.LabelTotalUangAnda.Text = "Label5"
+        '
+        'LabelRpTotalUangAnda
+        '
+        Me.LabelRpTotalUangAnda.AutoSize = True
+        Me.LabelRpTotalUangAnda.Location = New System.Drawing.Point(150, 134)
+        Me.LabelRpTotalUangAnda.Name = "LabelRpTotalUangAnda"
+        Me.LabelRpTotalUangAnda.Size = New System.Drawing.Size(39, 13)
+        Me.LabelRpTotalUangAnda.TabIndex = 8
+        Me.LabelRpTotalUangAnda.Text = "Label6"
+        '
+        'LabelRpBungaDeposito
+        '
+        Me.LabelRpBungaDeposito.AutoSize = True
+        Me.LabelRpBungaDeposito.Location = New System.Drawing.Point(150, 102)
+        Me.LabelRpBungaDeposito.Name = "LabelRpBungaDeposito"
+        Me.LabelRpBungaDeposito.Size = New System.Drawing.Size(39, 13)
+        Me.LabelRpBungaDeposito.TabIndex = 7
+        Me.LabelRpBungaDeposito.Text = "Label7"
+        '
+        'ltd4
+        '
+        Me.ltd4.AutoSize = True
+        Me.ltd4.Location = New System.Drawing.Point(142, 134)
+        Me.ltd4.Name = "ltd4"
+        Me.ltd4.Size = New System.Drawing.Size(39, 13)
+        Me.ltd4.TabIndex = 12
+        Me.ltd4.Text = "Label8"
+        '
+        'ltd3
+        '
+        Me.ltd3.AutoSize = True
+        Me.ltd3.Location = New System.Drawing.Point(142, 102)
+        Me.ltd3.Name = "ltd3"
+        Me.ltd3.Size = New System.Drawing.Size(39, 13)
+        Me.ltd3.TabIndex = 11
+        Me.ltd3.Text = "Label9"
+        '
+        'ltd2
+        '
+        Me.ltd2.AutoSize = True
+        Me.ltd2.Location = New System.Drawing.Point(142, 54)
+        Me.ltd2.Name = "ltd2"
+        Me.ltd2.Size = New System.Drawing.Size(45, 13)
+        Me.ltd2.TabIndex = 10
+        Me.ltd2.Text = "Label10"
+        '
+        'ltd1
+        '
+        Me.ltd1.AutoSize = True
+        Me.ltd1.Location = New System.Drawing.Point(142, 11)
+        Me.ltd1.Name = "ltd1"
+        Me.ltd1.Size = New System.Drawing.Size(45, 13)
+        Me.ltd1.TabIndex = 9
+        Me.ltd1.Text = "Label11"
+        '
+        'ButtonHitung
+        '
+        Me.ButtonHitung.Location = New System.Drawing.Point(214, 166)
+        Me.ButtonHitung.Name = "ButtonHitung"
+        Me.ButtonHitung.Size = New System.Drawing.Size(95, 35)
+        Me.ButtonHitung.TabIndex = 13
+        Me.ButtonHitung.Text = "Button1"
+        Me.ButtonHitung.UseVisualStyleBackColor = True
+        '
+        'BungaDeposito
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(325, 224)
+        Me.Controls.Add(Me.ButtonHitung)
+        Me.Controls.Add(Me.ltd4)
+        Me.Controls.Add(Me.ltd3)
+        Me.Controls.Add(Me.ltd2)
+        Me.Controls.Add(Me.ltd1)
+        Me.Controls.Add(Me.LabelRpTotalUangAnda)
+        Me.Controls.Add(Me.LabelRpBungaDeposito)
+        Me.Controls.Add(Me.LabelTotalUangAnda)
+        Me.Controls.Add(Me.LabelBungaDeposito)
+        Me.Controls.Add(Me.TextBoxJumlah)
+        Me.Controls.Add(Me.LabelJumlah)
+        Me.Controls.Add(Me.LabelPilihan)
+        Me.Controls.Add(Me.TextBoxJangkaWaktu)
+        Me.Controls.Add(Me.LabelJangkaWaktu)
+        Me.Name = "BungaDeposito"
+        Me.Text = "Bunga Deposito"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelJangkaWaktu As Label
+    Friend WithEvents TextBoxJangkaWaktu As TextBox
+    Friend WithEvents LabelPilihan As Label
+    Friend WithEvents LabelJumlah As Label
+    Friend WithEvents TextBoxJumlah As TextBox
+    Friend WithEvents LabelBungaDeposito As Label
+    Friend WithEvents LabelTotalUangAnda As Label
+    Friend WithEvents LabelRpTotalUangAnda As Label
+    Friend WithEvents LabelRpBungaDeposito As Label
+    Friend WithEvents ltd4 As Label
+    Friend WithEvents ltd3 As Label
+    Friend WithEvents ltd2 As Label
+    Friend WithEvents ltd1 As Label
+    Friend WithEvents ButtonHitung As Button
+End Class
---
+++ BungaDeposito.vb
@@ -0,0 +1,50 @@
+﻿Public Class BungaDeposito
+    Private Sub BungaDeposito_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .LabelJangkaWaktu.Text = "Jangka Waktu"
+            .LabelPilihan.Text = "Pilihan: 1, 3, 4, 12, 24"
+            .LabelJumlah.Text = "Jumlah"
+            .LabelBungaDeposito.Text = "Bunga Deposito"
+            .LabelTotalUangAnda.Text = "Total Uang Anda"
+            .ltd1.Text = ":"
+            .ltd2.Text = ":"
+            .ltd3.Text = ":"
+            .ltd4.Text = ":"
+            .ButtonHitung.Text = "Hitung"
+        End With
+    End Sub
+
+    Private Sub ButtonHitung_Click(sender As Object, e As EventArgs) Handles ButtonHitung.Click
+        Dim JangkaWaktu As Integer
+        Dim Uang, Bunga, TotalUang, Persen As Decimal
+
+        If TextBoxJangkaWaktu.Text = "" Then
+            MsgBox("Silahkan Isi Jangka Waktu!")
+        ElseIf TextBoxJumlah.Text = "" Then
+            MsgBox("Silahkan Isi Uang!")
+        Else
+
+            JangkaWaktu = TextBoxJangkaWaktu.Text
+            Uang = TextBoxJumlah.Text
+
+            Select Case JangkaWaktu
+                Case 1
+                    Persen = 5 / 100
+                Case 3
+                    Persen = 7 / 100
+                Case 6
+                    Persen = 8 / 100
+                Case 12
+                    Persen = 10 / 100
+                Case 24
+                    Persen = 24 / 100
+            End Select
+        End If
+
+        Bunga = Persen * Uang
+        TotalUang = Uang + Bunga
+
+        LabelBungaDeposito.Text = "Rp " & Bunga
+        LabelTotalUangAnda.Text = "Rp " & TotalUang
+    End Sub
+End Class
